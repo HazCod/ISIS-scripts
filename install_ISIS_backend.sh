0148fc3568e0ee36b5d121843733b37917e29247
HazCod <hazcod@outlook.be>
2014-02-10 14:32:15 +0100

    Added backend script.

---
+++ install_ISIS_backend.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+url="https://github.com/HazCod/ISIS-backend.git"
+dir=".isis"
+
+
+function installPackage {
+	sudo apt-get -q -y install $1
+	sudo -k
+}
+
+function installDependencies {
+	export DEBIAN_FRONTEND=noninteractive
+	
+	installPackage git
+	installPackage python
+	installPackage apache2
+	installPackage php
+}
+
+function getFromGit {
+	git clone $url $dir
+}
+
+
+# SCRIPT BEGIN
+
+if [ "$(id -u)" != "0" ]; then
+	echo "Please run this script as root."
+	exit 1
+fi
+
+installDependencies
+cd ~
+getFromGit
+
+# SCRIPT END
